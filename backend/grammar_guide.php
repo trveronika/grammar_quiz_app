9fd839b91af516afd6c3c6a9806eb72bb319779f
tári_veronika <tarivera1998@gmail.com>
2024-02-28 13:54:33 +0100

    Added base design for the grammar guide

---
+++ backend/grammar_guide.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="guide.css">
+    <title>Grammar Guide</title>
+</head>
+
+<body>
+    <h1>Grammar Guide</h1>
+
+    <details>
+        <summary>Verbs</summary>
+        <details>
+            <summary>Tenses</summary>
+            <p>
+                <div class="container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Tense</th>
+                                <th>Examples</th>
+                        <tbody>
+                            <tr>
+                                <td>Present Simple</td>
+                                <td>
+                                    Her name is Kate. <br>
+                                    I usually get up at 6 o’ clock.<br>
+                                    She goes to school every day.<br>
+                                    The Sun rises in the East.<br>
+                                    They never call us back.<br>
+                                    We don’t talk about negative topics.<br>
+                                    He doesn’t listen to pop music.<br>
+                                    Do you live in Hungary?<br>
+                                    Does she cook regularly?<br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Present Continuous</td>
+                                <td>
+                                    We are studying English at the moment.<br>
+                                    She is currently living in London.<br>
+                                    I am writing my test right now.<br>
+                                    Are you working on your computer?<br>
+                                    She is always singing. It’s so annoying.<br>
+                                    I usually eat sandwiches, but now I am eating apples.<br>
+                                    We are not listening to the radio right now.<br>
+                                    She is travelling to Mexico next week. She has the ticket.<br>
+                                    What are you doing next weekend?<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Present Perfect</td>
+                                <td>
+                                    I have lost my keys. Now I cannot enter my flat.<br>
+                                    We have never been to the USA.<br>
+                                    She has already closed the window, so you do not have to do that again.<br>
+                                    Have you ever eaten fish?<br>
+                                    Haven’t they bought the tickets yet?<br>
+                                    He has studied English for 10 years.<br>
+                                    I have lived in Romania since 2010.<br>
+                                    They have written 10 letters. The letters are finished.<br>
+                                    This author has written 10 novels so far.<br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Present Perfect Continuous</td>
+                                <td>
+                                    We have been working on this project for 10 months and it is still not finished.<br>
+                                    She has been living in Debrecen for 15 years. She still lives here.<br>
+                                    He looks very tired. He has been swimming for hours.<br>
+                                    They have been cooking, but they still need time.<br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Past Simple</td>
+                                <td>
+                                    Yesterday we went to school by bus.<br>
+                                    They moved abroad in 2023.<br>
+                                    Did you close the door after leaving?<br>
+                                    She sold her house 10 years ago.<br>
+                                    My mom used to drive me to school when I was a kid.<br>
+                                    The kids didn’t use to eat together during the day.<br>
+                                    At first we went shopping, then we ate at the mall and we finally drove home.<br>
+                                    She saw Peter last night.<br>
+                                    Jókai Mór wrote a lot of books. (He cannot write more)<br>
+                                    What did you do when the accident happened? (after the accident)<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Past Continuous</td>
+                                <td>
+                                    They were eating in the kitchen from 1 to 2.<br>
+                                    I was working out all afternoon.<br>
+                                    Was he sleeping all night.<br>
+                                    My mom was helping me with the dishes, while my brother was watching videos.<br>
+                                    You were taking a bath, when the post man arrived.<br>
+                                    What were you doing when the accident happened? (during the accident)<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Past Perfect</td>
+                                <td>
+                                    By the time we arrived at the railway station, the train had already left.<br>
+                                    Before she arrived, she had put on some makeup.<br>
+                                    We had eaten all the food by 2:15.<br>
+                                    He was happy, because he had passed the test.<br>
+                                    We couldn’t open the door because we had lost the keys.<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Past Perfect Continuous</td>
+                                <td>
+                                    She was tired because she had been working for 14 hours.<br>
+                                    He was living in Budapest for 4 years, before he moved to Debrecen.<br>
+                                    We sold the house. We had been considering it since 2015.<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Future Simple</td>
+                                <td>
+                                    That box looks heavy. I’ll help you carry it.<br>
+                                    She promises that she will love him forever.<br>
+                                    I will be 30 next year.<br>
+                                    It will probably rain next week.<br>
+                                    I won’t arrive on time because of the heavy traffic.<br>
+                                    Will you please help me with me homework?<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Future Continuous</td>
+                                <td>
+                                    This time next month I will be relaxing at home.<br>
+                                    Tomorrow at 10 she will be dancing with her friends at the disco.<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Future Perfect</td>
+                                <td>
+                                    She will have finished this project by next weekend.<br>
+                                    Will you have sold the car by this time next month?<br>
+                                    My parent will have been married for 10 years by this time next month.<br>
+                                    He will have left home by 9 o’ clock.<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Future Perfect Continuous</td>
+                                <td>
+                                    By the end of this year they will have been working at the same company for 10 years. <br>
+                                    By Friday I will have been writing this thesis for 6 month.<br>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Be going to</td>
+                                <td>
+                                    According to her planner she is going to meet her friends tomorrow.<br>
+                                    We are going to invite our whole family to our wedding.<br>
+                                    The sky looks grey, it’s going to rain.<br>
+                                    What are you going to do at the weekend?<br>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
+            </p>
+        </details>
+        <details>
+            <summary>Passives</summary>
+            <p>examples</p>
+        </details>
+        <details>
+            <summary>Modals</summary>
+            <p>examples</p>
+        </details>
+        <details>
+            <summary>Conditionals</summary>
+            <p>examples</p>
+        </details>
+        <details>
+            <summary>Reported speech</summary>
+            <p>examples</p>
+        </details>
+        <details>
+            <summary>Causatives</summary>
+            <p>examples</p>
+        </details>
+        <!-- <ul>
+            <li><strong>Tenses:</strong> Present, Past, Future</li>
+            <li><strong>Passives</strong></li>
+        </ul> -->
+    </details>
+
+    <details>
+        <summary>Nouns</summary>
+        <details>
+            <summary>Singular and Plural</summary>
+            <p>Explanation about singular and plural nouns.</p>
+        </details>
+        <details>
+            <summary>Countable and Uncountable</summary>
+            <p>Explanation about countable and uncountable nouns.</p>
+        </details>
+    </details>
+
+    <details>
+        <summary>Adjectives</summary>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+        <details>
+            <summary></summary>
+            <p></p>
+        </details>
+    </details>
+
+
+</body>
+
+</html>
